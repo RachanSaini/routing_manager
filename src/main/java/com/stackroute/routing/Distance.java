ff989f004b6a68e607b2f4deef22800921e0762f
Rachan <rachkau1@in.ibm.com>
2019-02-15 09:26:08 +0530

    added greedy algo

--- src/main/java/com/stackroute/routing/Distance.java
+++ src/main/java/com/stackroute/routing/Distance.java
@@ -9,7 +9,7 @@ public class Distance {
     private Random random = new Random(151190);
 
     //For demo hard coded parameters
-    int noOfContainers = 30;
+    int noOfOrders = 30;
     int noOfVehicles = 10;
     int vehicleCapacity = 50;
 
@@ -18,20 +18,16 @@ public class Distance {
     int depot_y = 50;
 
 
-
-    //Tabu parameter for tabu search
-    int tabu_horizon = 10;
-
-    public void calculateDistance(){
+    public double[][] calculateDistance(){
         System.out.println("inside distance class");
         //Initializing
         //creating random containers
-        Node nodes[] = new Node[noOfContainers + 1];
+        Node nodes[] = new Node[noOfOrders + 1];
         Node depot = new Node(depot_x,depot_y);
 
         nodes[0] = depot; //making first node as warehouse depot location.
 
-        for(int i = 1; i<= noOfContainers; i++) {
+        for(int i = 1; i<= noOfOrders; i++) {
             nodes[i] = new Node(i,
                     random.nextInt(100),
                     random.nextInt(100),
@@ -39,10 +35,10 @@ public class Distance {
             );
         }
 
-        double[][] distanceMatrix = new double[noOfContainers + 1][noOfContainers + 1];
+        double[][] distanceMatrix = new double[noOfOrders + 1][noOfOrders + 1];
         double delta_x,delta_y;
-        for(int i = 0; i<= noOfContainers; i++){
-            for(int j = i+1; j<=noOfContainers; j++){
+        for(int i = 0; i<= noOfOrders; i++){
+            for(int j = i+1; j<=noOfOrders; j++){
                 //depot to next neighbor
                 delta_x = (nodes[i].node_x - nodes[j].node_x);
                 delta_y = (nodes[i].node_y - nodes[j].node_y);
@@ -56,17 +52,17 @@ public class Distance {
             }
         }
 
-        int printMatrix = 1; //to print distance matrix.
-
-        if (printMatrix == 1){
-            for (int i =0; i<=noOfContainers; i++){
-                for (int j=0; j<=noOfContainers; j++){
+        double printMatrix[][] = new double[noOfOrders + 1][noOfOrders + 1]; //to print distance matrix.
+            for (int i =0; i<=noOfOrders; i++){
+                for (int j=0; j<=noOfOrders; j++){
+                     printMatrix[i][j] = distanceMatrix[i][j];
                     System.out.print(distanceMatrix[i][j] + "-");
                 }
                 System.out.println();
             }
-        }
 
+
+        return printMatrix;
     }
 
 
